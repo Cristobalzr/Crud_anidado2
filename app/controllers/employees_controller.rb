b06939b7424ddaf2c4ed35fc44f6812eb50cbd9d
Cristobal <cristobalz@outlook.com>
2019-06-19 19:48:55 -0400

    "listo hasta el create de employee"

---
+++ app/controllers/employees_controller.rb
@@ -0,0 +1,10 @@
+class EmployeesController < ApplicationController
+  def create
+    @employee = Employee.new(employee_params)
+    @employee.save
+    redirect_to company_path(params[:company_id])
+  end
+  def employee_params
+    params.require(:employee).permit(:first_name, :last_name, :email)
+  end
+end
