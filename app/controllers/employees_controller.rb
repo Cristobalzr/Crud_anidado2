ea5f5d7fcfd9b960d815b7f7dfc5f9b57d8dc351
Cristobal <cristobalz@outlook.com>
2019-06-19 21:26:42 -0400

    cantidad de empleados por compañia en el show

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -4,7 +4,14 @@ class EmployeesController < ApplicationController
     @employee.save
     redirect_to company_path(params[:company_id])
   end
+
+    def destroy
+      @employee = Employee.find(params[:id])
+      @employee.destroy
+      redirect_to @employee.company
+    end
+
   def employee_params
-    params.require(:employee).permit(:first_name, :last_name, :email, :company_id)
+    params.require(:employee).permit(:first_name, :last_name, :email, :company_id, :area_id)
   end
 end
